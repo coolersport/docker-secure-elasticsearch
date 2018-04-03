9f0080914776a5159cfdfd1a69970bc066f5acee
Tien Tran <tiendung.tran@gmail.com>
2018-04-03 16:35:55 +1000

    Suppress error if chown fails

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 if [[ "$@" == 'eswrapper' ]]; then
         find /usr/share/elasticsearch/config -type f -exec chmod 600 {} ';' &> /dev/null
         find /usr/share/elasticsearch/config -type d -exec chmod 700 {} ';' &> /dev/null
-        chown -R elasticsearch /usr/share/elasticsearch/config
+        chown -R elasticsearch /usr/share/elasticsearch/config 2> /dev/null
 fi
 
 exec /usr/local/bin/docker-entrypoint.sh "$@"
