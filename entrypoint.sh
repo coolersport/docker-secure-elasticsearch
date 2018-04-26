4afd20f96c1fd0483e18d0b811b606ac4effd540
Tien Tran <ttran@genixventures.com>
2018-04-26 11:43:58 +1000

    Placed generated keystore under config folder

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,10 +6,11 @@ if [[ -f "$CA_CERTIFICATE" ]]; then
 	    -file $CA_CERTIFICATE -alias custom-root-ca -noprompt >/dev/null
 
 	if [[ -f "$CERTIFICATE" && -f "$CERTIFICATE_KEY" ]]; then
+		mkdir -p /usr/share/elasticsearch/config/certs
 		openssl pkcs12 -export -in $CERTIFICATE -inkey $CERTIFICATE_KEY \
 		    -out keystore.p12 -CAfile $CA_CERTIFICATE -caname "Root CA" -password pass:$STORE_PASS
 		keytool -importkeystore \
-		    -deststorepass $STORE_PASS -destkeypass $KEY_PASS -destkeystore /keystore.jks \
+		    -deststorepass $STORE_PASS -destkeypass $KEY_PASS -destkeystore /usr/share/elasticsearch/config/certs/keystore.jks \
 	        -srckeystore keystore.p12 -srcstoretype PKCS12 -srcstorepass $STORE_PASS
 	    rm -rf keystore.p12
 	fi
