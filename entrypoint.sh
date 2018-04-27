0d680d42f4e261cea9065561dc500abdedeced72
Tien Tran <tiendung.tran@gmail.com>
2018-04-27 12:31:23 +1000

    Ensure elasticsearch data folder owned by the user

--- entrypoint.sh
+++ entrypoint.sh
@@ -20,6 +20,7 @@ if [[ "$@" == 'eswrapper' ]]; then
         find /usr/share/elasticsearch/config -type f -exec chmod 600 {} ';' &> /dev/null
         find /usr/share/elasticsearch/config -type d -exec chmod 700 {} ';' &> /dev/null
         chown -R elasticsearch /usr/share/elasticsearch/config 2> /dev/null
+        chown elasticsearch /usr/share/elasticsearch/data 2> /dev/null
 fi
 
 exec /usr/local/bin/docker-entrypoint.sh "$@"
