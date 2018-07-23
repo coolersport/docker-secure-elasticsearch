28df3270f1a3efa5122a00b73f6846c27a4bba3c
Tien Tran <ttran@genixventures.com>
2018-07-23 11:55:10 +1000

    Updated path to keytool as it's no longer in system path

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,14 +2,14 @@
 #set -e # chown may fail on readonly mounts
 
 if [[ -f "$CA_CERTIFICATE" ]]; then
-	keytool -import -keystore ${JAVA_HOME}/lib/security/cacerts -storepass changeit \
+	$JAVA_HOME/bin/keytool -import -keystore ${JAVA_HOME}/lib/security/cacerts -storepass changeit \
 	    -file $CA_CERTIFICATE -alias custom-root-ca -noprompt >/dev/null
 
 	if [[ -f "$CERTIFICATE" && -f "$CERTIFICATE_KEY" ]]; then
 		mkdir -p /usr/share/elasticsearch/config/certs
 		openssl pkcs12 -export -in $CERTIFICATE -inkey $CERTIFICATE_KEY \
 		    -out keystore.p12 -CAfile $CA_CERTIFICATE -caname "Root CA" -password pass:$STORE_PASS
-		keytool -importkeystore \
+		$JAVA_HOME/bin/keytool -importkeystore \
 		    -deststorepass $STORE_PASS -destkeypass $KEY_PASS -destkeystore /usr/share/elasticsearch/config/certs/keystore.jks \
 	        -srckeystore keystore.p12 -srcstoretype PKCS12 -srcstorepass $STORE_PASS
 	    rm -rf keystore.p12
